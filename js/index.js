d44683a1776324de28c9423c9e8acbebfe06ae61
Mike Hendriks <mikehen6@gmail.com>
2020-04-07 14:27:40 +0200

    :bug: Push notification test 2

---
+++ js/index.js
@@ -0,0 +1,78 @@
+
+//Setup libraries we want to use (installed them first with *npm install*)
+const webpush = require("web-push");
+const fetch = require("node-fetch");
+const prompts = require("prompts");
+
+//Location of your subscribers file (normally they would be in a database)
+const yourSubscriberJSONFileURL =
+  "HTTPS://https://fervent-knuth-4b2b3e.netlify.com/data/subscribers.json)";
+
+//Place your keys here
+webpush.setVapidDetails(
+  "mailto:mikehen6@gmail.com", //Subject VAPID
+  "BK2cTqo2duEEBRo2LBBoaDvrVo_prXTVBTXtqzLNd9IaANIkJGikJPmc_xllVakoNERWGNTHFLbXQRfujlzv1fM", // Public Key VAPID
+  "-13zLpiMGLoqC3Z5wKtDSbSrxt9KR3u41RSXORb6_tU" //Private Key VAPID
+);
+
+console.log(
+  " ==========================================\n",
+  "==       SEND PUSH NOTIFICATION         ==\n",
+  "==========================================\n"
+);
+
+//This allows you to fill in the titel and message of the notification in the CLI
+const questions = [
+  {
+    type: "text",
+    name: "title",
+    message: "Push notification title"
+  },
+  {
+    type: "text",
+    name: "message",
+    message: "Push notification message"
+  }
+];
+
+(async () => {
+  const answers = await prompts(questions);
+
+  console.log(
+    "\n ==========================================\n",
+    "==    SENDING MESSAGE TO SUBSCRIBERS    ==\n",
+    "==========================================\n"
+  );
+
+  //Putting the promted title and message in variables to use
+  let pushTitle = answers.title;
+  let pushMessage = answers.message;
+
+  //Send a notification to every subscriber
+  fetch(yourSubscriberJSONFileURL)
+    .then(subscriberJSON => subscriberJSON.json())
+    .then(subscriberJSON => {
+      for (let subscriberEndpoint in subscriberJSON) {
+        //Setting up format of subcription for sending
+        const pushSubscription = {
+          endpoint: subscriberEndpoint,
+          keys: {
+            auth: subscriberJSON[subscriberEndpoint]["keys"]["auth"],
+            p256dh: subscriberJSON[subscriberEndpoint]["keys"]["p256dh"]
+          }
+        };
+        //Actual sending
+        webpush
+          .sendNotification(
+            pushSubscription,
+            `{"title":"${pushTitle}","message":"${pushMessage}"}`
+          )
+          .then(result => {
+            console.log(`-- Message send to ${pushSubscription.endpoint}`);
+          })
+          .catch(error => {
+            console.log(`-- Message NOT send to ${pushSubscription.endpoint}`);
+          });
+      }
+    });
+})();
